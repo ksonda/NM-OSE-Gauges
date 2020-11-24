00128a5f72d3c06d892643a197ef463fc8b4cc45
Kyle Onda <kso8@CDSS-6342.local>
2020-11-24 14:13:49 -0700

    update

--- ose_gage_connect.R
+++ ose_gage_connect.R
@@ -1,8 +1,5 @@
 ##########################
 # Purpose: Link features in OSE_Gauges.zip to Gage landing pages at meas.ose.state.nm.us
-# Author: Kyle Onda
-#
-#
 ##########################
 
 
@@ -11,11 +8,40 @@
 library(httr)
 library(xml2)
 library(sf)
+library(dplyr)
+library(fuzzyjoin)
 
+#Make link scrape function
+##########################################
+scrapelinks <- function(url){
+  # Create an html document from the url
+  webpage <- xml2::read_html(url)
+  # Extract the URLs
+  url_ <- webpage %>%
+    rvest::html_nodes("a") %>%
+    rvest::html_attr("href")
+  # Extract the link text
+  link_ <- webpage %>%
+    rvest::html_nodes("a") %>%
+    rvest::html_text()
+  return(tibble(link = link_, url = url_))
+}
+############################################
 
 # Import data
 unzip ("data/OSE_Gauges.zip", exdir="data")
-gages <- read_sf("data/OSE_Gauges/NM2015_Gauges_OSE.shp")
+gages <- sf::read_sf("data/OSE_Gauges/NM2015_Gauges_OSE.shp")
+districts <- sf::read_sf("data/OSE_District_Boundary-shp/OSE_Districts.shp")
+# Parse website into table of gauge names and landing page URLs
+links <- scrapelinks(url)
+links <- links %>% filter(substr(url,1,5)=="site.") %>%
+  mutate(url = paste0(.GlobalEnv$url,url)) %>%
+  mutate(link = gsub("\r\n","",link)) %>%
+  mutate(link = gsub(" ","",link))
 
-# Parse website
-url <- 
+#
+links$name <- links$link
+gages$name <- gages$Gauge_name
+gages <- filter(gages,!is.na(name))
+gages$geometry<-NULL
+Test <- stringdist_left_join(links,gages,by="name",method="soundex")
